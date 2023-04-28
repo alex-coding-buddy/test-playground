96162ae78e8786a30d2d6dac259848299a1763bc
Oleksandr Shtaniuk <oleksandr.shtaniuk@gmail.com>
2023-04-28 15:48:59 +0200

    Task 6

--- src/main/java/com/infy/testplayground/controller/RelationController.java
+++ src/main/java/com/infy/testplayground/controller/RelationController.java
@@ -7,8 +7,12 @@ import com.infy.testplayground.repo.RelationRepository;
 import com.infy.testplayground.service.RelationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
 import java.util.Collection;
 
 import static com.infy.testplayground.dto.InverseDto.Inverse.NO;
@@ -21,7 +25,7 @@ public class RelationController extends AbstractCrudController<RelationRepositor
     private final RelationService relationService;
 
     @PostMapping("/task1")
-    public HttpStatus createCustom(@RequestBody RelationDto dto) {
+    public HttpStatus createCustom(@RequestBody @Valid RelationDto dto) {
         relationService.createCustomRelation(dto);
         return HttpStatus.CREATED;
     }
@@ -34,5 +38,11 @@ public class RelationController extends AbstractCrudController<RelationRepositor
     @GetMapping("/inverse")
     public Collection<?> getInversed() {
         return relationService.getInverse();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/infy/testplayground/dto/RelationDto.java
+++ src/main/java/com/infy/testplayground/dto/RelationDto.java
@@ -4,11 +4,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 @Accessors(chain = true)
 @NoArgsConstructor
 public class RelationDto {
+
+    @Pattern(regexp="^[a-zA-Z]",message="User-friendly message")
     private String w1;
+    @Pattern(regexp="^[a-zA-Z]",message="User-friendly message")
     private String w2;
     private String r;
 }
